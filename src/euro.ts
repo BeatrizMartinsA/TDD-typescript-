46e0808984889e3db4df80cd93325619ea12dbc0
BeatrizMartinsA <beatriz.angelo99@gmail.com>
2021-08-02 20:49:33 -0300

    feat: class money and class euro

---
+++ src/euro.ts
@@ -0,0 +1,13 @@
+import { Money } from "./money"
+
+export class Euro extends Money {
+
+    constructor(amount: number) {
+        super()
+        super.amount = amount
+    }
+
+    times (multiplier: number): Euro {
+        return new Euro(this.amount * multiplier)
+    }
+}
---
+++ src/money.spec.ts
@@ -0,0 +1,30 @@
+import { Dollar } from "./dollar"
+import  { Euro } from "./euro"
+
+describe('Money', () => {
+    it('should correctly hanlde dollar multiplication', () => {
+        const five: Dollar = new Dollar(5)
+        expect(five.times(2).equals(new Dollar(10))).toBeTruthy()
+        expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
+    })
+
+    it('should correctly hanlde euro multiplication', () => {
+        const five: Euro = new Euro(5)
+        expect(five.times(2).equals(new Euro(10))).toBeTruthy()
+        expect(five.times(3).equals(new Euro(15))).toBeTruthy()
+    })
+
+    it('should correctly handle dollar equality', () => {
+        expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
+        expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy()
+    })
+
+    it('should correctly handle euro equality', () => {
+        expect(new Euro(5).equals(new Euro(5))).toBeTruthy()
+        expect(new Euro(5).equals(new Euro(6))).toBeFalsy()
+    })
+
+    it('should correctly handle money equality', () => {
+        expect(new Euro(5).equals(new Dollar(5))).toBeFalsy()
+    })
+})
